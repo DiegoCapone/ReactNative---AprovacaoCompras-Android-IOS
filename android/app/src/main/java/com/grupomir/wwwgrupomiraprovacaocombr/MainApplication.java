37adbcbc40322917707a0e7e82e3ca997d65093b
Diego <playskil@gmail.com>
2019-06-17 11:13:54 -0300

    Dependencias instaladas

--- android/app/src/main/java/com/grupomir/wwwgrupomiraprovacaocombr/MainApplication.java
+++ android/app/src/main/java/com/grupomir/wwwgrupomiraprovacaocombr/MainApplication.java
@@ -3,6 +3,7 @@ package com.grupomir.wwwgrupomiraprovacaocombr;
 import android.support.multidex.MultiDexApplication;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.swmansion.reanimated.ReanimatedPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -44,6 +45,7 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
+            new LinearGradientPackage(),
             new ReanimatedPackage(),
             new RNGestureHandlerPackage(),
             new VectorIconsPackage(),
